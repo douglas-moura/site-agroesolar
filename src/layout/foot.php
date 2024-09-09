575634cbe66aa6164cc9a398cd49eb52fa7a9af0
Douglas Moura <douglas12345.dsm@gmail.com>
2024-09-09 14:40:43 -0300

    sessão contato e banner

--- src/layout/foot.php
+++ src/layout/foot.php
@@ -2,7 +2,7 @@
         <footer>
             <section>
                 <h5>
-                    Agro & Solar - Painéis Fotovoltáicos | 2024
+                    <strong>Agro & Solar</strong> - Painéis Fotovoltáicos | 2024
                     <br>
                     Todos os direitos Reservados. CNPJ 12.345.789/0001-10 / São Paulo - SP
                 </h5>
--- src/pages/home.php
+++ src/pages/home.php
@@ -1,21 +1,16 @@
 <?php include_once './src/layout/banner.php'; ?>
 <section id="sctn-emp">
+    <h2>Sobre a Empresa</h2>
     <div>
-        <div class="col-50">
-            <h2>Sobre a Empresa</h2>
-        </div>
-        <div class="col-50">
-            <p>Somos totalmente voltados à solução dos principais problemas da Humanidade – alimentos e energia. Desenvolvemos estudos de retorno financeiro e providenciamos desde o projeto das estruturas, seu financiamento, instalação e manutenção, além da venda da energia excedente. Defendemos o uso compartilhado do solo para a produção de alimentos e energia elétrica através de uma solução a que chamamos AGROE SOLAR. Para isso desenvolvemos soluções que permitem, a partir de uma área de terra obter:</p>
-            <br>
-            <ul>
-                <li>alto rendimento de culturas;</li>
-                <li>geração de energia elétrica fotovoltaíca com alto rendimento;</li>
-                <li>irrigação com redução de consumo específico de água para as plantas;</li>
-                <li>recuperação de águas de chuva.</li>
-                <li>iluminação complementar para aumento do período de fotossíntese e redução de período de plantio.</li>
-                <li>alinhados às principais espectativas de ESG, defendemos um mundo novo onde a agricultura e a geração de energia se encontrem em simbiose, altamente proveitosa para as duas atividades, aumentando significativamente o rendimento financeiro da terra, criando bem estar para plantas e animais e um circulo virtuoso de desenvolvimento sustentável.</li>
-            </ul>
-        </div>
+        <p class="col-50">Somos totalmente voltados à solução dos principais problemas da Humanidade – alimentos e energia. Desenvolvemos estudos de retorno financeiro e providenciamos desde o projeto das estruturas, seu financiamento, instalação e manutenção, além da venda da energia excedente. Defendemos o uso compartilhado do solo para a produção de alimentos e energia elétrica através de uma solução a que chamamos AGROE SOLAR. Para isso desenvolvemos soluções que permitem, a partir de uma área de terra obter:</p>
+        <ul class="col-50">
+            <li>Alto rendimento de culturas;</li>
+            <li>Geração de energia elétrica fotovoltaíca com alto rendimento;</li>
+            <li>Irrigação com redução de consumo específico de água para as plantas;</li>
+            <li>Recuperação de águas de chuva.</li>
+            <li>Iluminação complementar para aumento do período de fotossíntese e redução de período de plantio.</li>
+            <li>Alinhados às principais espectativas de ESG, defendemos um mundo novo onde a agricultura e a geração de energia se encontrem em simbiose, altamente proveitosa para as duas atividades, aumentando significativamente o rendimento financeiro da terra, criando bem estar para plantas e animais e um circulo virtuoso de desenvolvimento sustentável.</li>
+        </ul>
     </div>
 </section>
 <!--
@@ -45,32 +40,29 @@
     </div>
 </section>
 -->
-<section id="sctn-agr">
-    <h2>Título</h2>
+<section id="sctn-agr" class="bg-cinza">
+    <h2>Agro</h2>
     <div>
-        <div class="col-50">
-            <h3>Agro</h3>
-        </div>
         <div class="col-50">
             <p>A energia solar permite que produtores rurais reduzam seus custos com eletricidade, aumentem a sustentabilidade de suas operações e garantam maior independência energética, especialmente em áreas remotas onde o acesso à rede elétrica é limitado ou inexistente. Além disso, o uso de energia solar no agro pode contribuir para a redução das emissões de carbono, promovendo práticas agrícolas mais sustentáveis.</p>
         </div>
     </div>
 </section>
 <section id="sctn-res">
+    <h2>Residencial</h2>
     <div>
         <div class="col-50">
-            <h2>Residencial</h2>
+            <p>A instalação de sistemas fotovoltaicos em residências permite aos moradores reduzir significativamente suas contas de luz, uma vez que a energia gerada pode suprir parte ou toda a demanda elétrica da casa. Além disso, qualquer excedente de energia produzido pode ser injetado na rede elétrica, gerando créditos que podem ser utilizados em momentos de menor geração, como à noite.</p>
         </div>
         <div class="col-50">
-            <p>A instalação de sistemas fotovoltaicos em residências permite aos moradores reduzir significativamente suas contas de luz, uma vez que a energia gerada pode suprir parte ou toda a demanda elétrica da casa. Além disso, qualquer excedente de energia produzido pode ser injetado na rede elétrica, gerando créditos que podem ser utilizados em momentos de menor geração, como à noite.</p>
             <p>A procura por energia solar no mercado residencial tem crescido devido a fatores como a queda nos custos dos equipamentos, a disponibilidade de linhas de financiamento e a conscientização sobre a importância de adotar práticas mais sustentáveis. Além de proporcionar economia, a instalação de painéis solares valoriza o imóvel e contribui para a redução das emissões de gases de efeito estufa, tornando a residência mais ecológica.</p>
         </div>
     </div>
 </section>
-<section id="sctn-peq">
+<section id="sctn-peq" class="bg-">
+    <h2>Pequenos Negócios</h2>
     <div>
         <div class="col-50">
-            <h2>Pequenos Negócios</h2>
         </div>
         <div class="col-50">
             <p>No mercado de pequenos negócios, a instalação de painéis fotovoltaicos pode oferecer soluções personalizadas que ajudam empresas de menor porte a reduzir seus custos operacionais e aumentar a sustentabilidade de suas operações. Pequenos negócios, como lojas, escritórios, restaurantes, e pequenas indústrias, podem se beneficiar ao instalar sistemas solares, pois isso permite que eles gerem parte ou toda a energia que consomem, diminuindo significativamente suas contas de eletricidade.</p>
@@ -78,13 +70,28 @@
     </div>
 </section>
 <section id="sctn-grd">
+    <h2>Grandes Negócios</h2>
     <div>
         <div class="col-50">
-            <h2>Grandes Negócios</h2>
+            <p>No mercado de grandes negócios, uma empresa de instalação de painéis fotovoltaicos pode desempenhar um papel crucial na transição para fontes de energia mais sustentáveis e na redução dos custos operacionais dessas organizações. Grandes empresas, como indústrias, centros de distribuição, shoppings, e corporações com múltiplas unidades, têm um consumo energético elevado, o que faz da energia solar uma solução atraente para diminuir as despesas com eletricidade e melhorar a sustentabilidade corporativa.</p>
         </div>
         <div class="col-50">
-            <p>No mercado de grandes negócios, uma empresa de instalação de painéis fotovoltaicos pode desempenhar um papel crucial na transição para fontes de energia mais sustentáveis e na redução dos custos operacionais dessas organizações. Grandes empresas, como indústrias, centros de distribuição, shoppings, e corporações com múltiplas unidades, têm um consumo energético elevado, o que faz da energia solar uma solução atraente para diminuir as despesas com eletricidade e melhorar a sustentabilidade corporativa.</p>
             <p>Para grandes negócios, a instalação de sistemas fotovoltaicos em larga escala pode não apenas atender a uma parte significativa da demanda energética, mas também possibilitar a implementação de microgrids, que aumentam a resiliência energética da empresa e reduzem a dependência da rede elétrica pública. Além disso, ao gerar energia limpa, essas empresas podem reduzir suas emissões de carbono, cumprindo metas ambientais e regulatórias, e reforçando sua imagem de responsabilidade social e ambiental.</p>
+        </div>
+    </div>
+</section>
+<section id="sctn-cnt" class="bg-">
+    <h2>Entre em Contato</h2>
+    <div>
+        <div class="col-40">
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                contato@email.com.br
+            </p>
+            <p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
+                +55 11 99999-9999
+            </p>
         </div>
     </div>
 </section>
