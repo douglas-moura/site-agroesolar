d5d84260327b7a1a6c379cd7594e4522c1149d49
Douglas Moura <douglas12345.dsm@gmail.com>
2024-12-12 17:45:20 -0300

    feat: Landinga Page - Locação de Sistemas
    - pasta /pages criada em /assets
    - locacao-sistemsa.html criado em /pages
    - webpack.config.js alterado para gerar a pagina da lp
    - favicon.ico movido para /src
    
    Landing Page:
    - banner criado com base na referÇencia do Bradesco

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: "development", // Pode ser 'development' ou 'production'
-    entry: "./src/index.ts", // Arquivo de entrada principal
+    mode: 'development', // Pode ser 'development' ou 'production'
+    entry: './src/index.ts', // Arquivo de entrada principal
     output: {
-        filename: "index.js", // Nome do arquivo gerado
-        path: path.resolve(__dirname, "dist"), // Pasta de saída
+        filename: 'index.js', // Nome do arquivo gerado
+        path: path.resolve(__dirname, 'dist'), // Pasta de saída
         clean: true, // Limpa a pasta `dist` antes de cada build
     },
     module: {
@@ -33,14 +33,14 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/, // Processa imagens
-                type: "asset/resource",
+                type: 'asset/resource',
                 generator: {
                     filename: 'img/[name][ext]', // Pasta de saída para imagens
                 },
             },
             {
                 test: /\.html$/, // Processa arquivos HTML
-                use: ["html-loader"],
+                use: ['html-loader'],
             },
             {
                 test: /\.tsx?$/, // Para arquivos .ts e .tsx
@@ -51,11 +51,16 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/index.html", // Arquivo HTML base
-            filename: "index.html", // Nome do arquivo de saída
+            favicon: './src/favicon.ico', // Caminho do favicon
+            template: './src/index.html', // Arquivo HTML base
+            filename: 'index.html', // Nome do arquivo de saída
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pages/locacao-de-sistemas.html', // Caminho do arquivo da página
+            filename: 'pages/locacao-de-sistemas.html', // Nome do arquivo de saída
         }),
         new MiniCssExtractPlugin({
-            filename: "styles.css", // Nome do CSS extraído
+            filename: 'styles.css', // Nome do CSS extraído
         }),
         new CopyWebpackPlugin({
             patterns: [
@@ -64,8 +69,9 @@ module.exports = {
         }),
     ],
     devServer: {
-        static: "./dist", // Pasta servida pelo dev server
+        static: './dist', // Pasta servida pelo dev server
         port: 3000, // Porta do servidor local
+        open: true,
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'], // Permite importar arquivos sem especificar a extensão
