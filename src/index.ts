d980c1f66284bd59eb1dea677b4d54b57d8bedab
Douglas Moura <douglas12345.dsm@gmail.com>
2024-12-12 12:52:21 -0300

    fix:    Correção no ts
    - Função de abrir e fechar menu mobile revisado

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import './assets/css/styles.css'
 
-console.log('foi')
-
-function abrirMenuMobile() {
+function abrirMenuMobile(): void {
     const menuMobile = document.getElementById('menu-mobile') as HTMLDivElement
     menuMobile.style.transform = 'translateX(0)'
 }
 
-function fecharMenuMobile() {
-    const menuMobile = document.getElementById('menu-mobile')
+function fecharMenuMobile(): void {
+    const menuMobile = document.getElementById('menu-mobile') as HTMLDivElement
     setTimeout(() => {
-        menuMobile!.style.transform = 'translateX(100%)'
+        menuMobile.style.transform = 'translateX(100%)'
     }, 200)
 }
